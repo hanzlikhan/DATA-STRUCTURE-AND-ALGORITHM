5b352411a3db1623ed0327c4f9e797caba8f6db8
hanzlikhan <khangormani79@gmail.com>
2024-03-16 22:00:17 +0500

    initialize data structure

---
+++ SORTING/bubbleSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {12, 78, 34, 97, 22, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << "the sorted array is : " << arr[i] << " ";
+    }
+    cout << endl;
+}
